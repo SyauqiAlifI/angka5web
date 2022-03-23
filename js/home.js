23c4debc94b3ef5fcc2446d9bfd495944d7d47d9
SyauqiAlifI <vstudio776@gmail.com>
2022-03-23 11:24:01 +0700

    yay

--- js/home.js
+++ js/home.js
@@ -1,3 +1,6 @@
-var video = document.getElementById("header-photo");
-video.play();
-video.loop = true;
+// make a script while loading the page that will make a loading
+// When the page is loaded, the loading will be removed
+function loading() {
+    var loading = document.getElementById("loading");
+    loading.style.display = "none";
+}
